b0ad2fc9dd6fd80290492d8bec89a5f8fbd7a43c
HuongHTL1909 <hothilanhuong1909@gmail.com>
2023-07-21 00:47:41 +0700

    Commit changes for register.blade.php

--- resources/views/frontend/pages/register.blade.php
+++ resources/views/frontend/pages/register.blade.php
@@ -150,7 +150,7 @@
                 }
             });
 
-            //Initialize Select2 Elements
+            //Initialize Select2 Elements.
             $('.select2').select2();
         });
     </script>
